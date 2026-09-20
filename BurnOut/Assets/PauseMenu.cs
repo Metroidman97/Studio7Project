f18c625dd1d96ae4c2a530c11ef9f1bfd2f8944e
3randon000 <138661435+3randon000@users.noreply.github.com>
2026-09-20 15:57:30 -0400

    core pause menu functionality implemented

---
+++ BurnOut/Assets/PauseMenu.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void Return()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1;
+    }
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
